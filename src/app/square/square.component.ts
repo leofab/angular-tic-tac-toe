339c5e2a1e086daef6c21ec1c0e8b14880de7e2c
Léo Fabrício <leofabr3@gmail.com>
2022-02-01 18:56:20 -0300

    nebula imports NbButtonModule

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-      <button style="width:200px;height:200px;font-size:40px">{{ value }}</button>
+      <button nbButton style="width:200px;height:200px;font-size:40px">{{ value }}</button>
   `,
   styles: [
     
