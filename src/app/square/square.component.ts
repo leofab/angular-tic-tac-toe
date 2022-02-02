a2031921d0c38b63a4eb3fb87c6fe54a1bcd8835
Léo Fabrício <leofabr3@gmail.com>
2022-02-01 21:32:56 -0300

    implement ng add @angular/pwa

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,9 +3,11 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-      <button nbButton style="width:200px;height:200px;font-size:40px">{{ value }}</button>
+      <button nbButton *ngIf="!value">{{ value }}</button>
+      <button nbButton hero status="sucess" *ngIf="value == 'X'">{{ value }}</button>
+      <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
   `,
-  styles: [
+  styles: [ ''
     
   ]
 })
